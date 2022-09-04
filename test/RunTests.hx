b56ccd7a382dd323e13a33fdcd2891be8657e035
jobf <james@owlbee.net>
2022-09-04 14:02:05 +0100

    tests compile again

--- test/RunTests.hx
+++ test/RunTests.hx
@@ -2,19 +2,17 @@ package test;
 
 import test.SysExTests;
 import test.DisplayTests;
-import test.LedTests;
 import utest.ui.Report;
 import utest.Runner;
 
 class RunTests {
-    public static function main() {
-        var runner = new Runner();
-        runner.addCase(new SysExColorTests());
-        runner.addCase(new SysExSetPadStateTests());
-        runner.addCase(new SysExSetOledStateTests());
-        runner.addCase(new DisplayTests());
-        runner.addCase(new LedTests());
-        Report.create(runner);
-        runner.run();
-      }
+	public static function main() {
+		var runner = new Runner();
+		runner.addCase(new SysExColorTests());
+		runner.addCase(new SysExSetPadStateTests());
+		runner.addCase(new SysExSetOledStateTests());
+		runner.addCase(new DisplayTests());
+		Report.create(runner);
+		runner.run();
+	}
 }
